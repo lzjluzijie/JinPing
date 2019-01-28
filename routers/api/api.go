6d333c6982040fda081ee0ac1dd0ce9dbca28994
halulu <i@halu.lu>
2019-01-28 10:12:07 +0800

    register

--- routers/api/api.go
+++ routers/api/api.go
@@ -5,7 +5,7 @@ import "gopkg.in/macaron.v1"
 func RegisterRouters(m *macaron.Macaron) {
 	m.Group("/api", func() {
 		m.Post("/comment", NewComment)
-		m.Post("/user", NewUser)
+		m.Post("/user/register", NewUser)
 		m.Post("/topic", NewTopic)
 		m.Get("/comments", GetCommentsByURL)
 	})
